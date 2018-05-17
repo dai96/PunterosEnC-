a59492a7decc576a091c3b1ea0c3423394fd4a98
dai96 <dairacomecerebros@gamil.com>
2018-05-17 10:38:18 -0300

    swappunteros

---
+++ punteros/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//void intercambiar(int num1, int num2);
+void intercambiarP(int *num1, int *num2);
+
+
+
+int main()
+{
+    int edadUno;
+    int edadDos;
+
+    edadUno=22;
+    edadDos=99;
+
+    //intercambiar (edadUno, edadDos);
+    intercambiarP (&edadUno, &edadDos);
+
+    printf("\n edad 1: %d ", edadUno);
+    printf("\n edad 2: %d ", edadDos);
+
+
+
+
+    return 0;
+}
+
+
+
+/**void intercambiar(int num1, int num2)
+{
+    int aux;
+    aux=num1;
+    num1=num2;
+    num2=aux;
+}
+*/
+
+void intercambiarP (int *num1, int *num2)
+{
+    int aux;
+    aux=*num1;
+    *num1=*num2;
+    *num2=aux;
+}
